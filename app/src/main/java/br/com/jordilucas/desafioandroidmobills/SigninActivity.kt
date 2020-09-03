41607ca20ca40dd39428b4b4d7c328eea6b268a6
Jordi <jordilucas3@gmail.com>
2020-09-03 13:43:29 -0300

    Adcionado firebaseAuth

--- app/src/main/java/br/com/jordilucas/desafioandroidmobills/SigninActivity.kt
+++ app/src/main/java/br/com/jordilucas/desafioandroidmobills/SigninActivity.kt
@@ -3,7 +3,7 @@ package br.com.jordilucas.desafioandroidmobills
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class SigninActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
