31b13c2cfbfc6d2c51546e5500ea5a2f57fc65cd
Jordi <jordilucas3@gmail.com>
2020-09-04 14:14:47 -0300

    activity base pra verificar se tem usuario logado

--- app/src/main/java/br/com/jordilucas/desafioandroidmobills/BaseActivity.kt
+++ app/src/main/java/br/com/jordilucas/desafioandroidmobills/BaseActivity.kt
@@ -1,52 +1,42 @@
 package br.com.jordilucas.desafioandroidmobills
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.MenuItem
-import androidx.fragment.app.Fragment
-import com.google.android.material.bottomnavigation.BottomNavigationView
-import com.google.android.material.navigation.NavigationView
-import kotlinx.android.synthetic.main.activity_base.*
+import com.google.firebase.auth.FirebaseAuth
 
-class BaseActivity : AppCompatActivity(), BottomNavigationView.OnNavigationItemSelectedListener  {
+open class BaseActivity : AppCompatActivity() {
 
+    protected val fbAuth:FirebaseAuth = FirebaseAuth.getInstance()
+    private var hasInitCalled = false
 
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_base)
-        bottomNav.setOnNavigationItemSelectedListener(this)
-        newFragment(HomeFragment())
-    }
-
-
-
-    override fun onNavigationItemSelected(item: MenuItem): Boolean {
-        when(item.itemId){
-            R.id.home_nav ->{
-                newFragment(HomeFragment())
-                return true
-            }
-            R.id.receita_nav ->{
-                newFragment(ReceitaFragment())
-                return true
-            }
-            R.id.despesa_nav ->{
-                newFragment(DespesaFragment())
-                return true
-            }
-            R.id.conta_nav ->{
-                newFragment(MinhaContaFragment())
-                return true
+    private var authListener: FirebaseAuth.AuthStateListener =
+        FirebaseAuth.AuthStateListener { firebaseAuth ->
+            val user = firebaseAuth.currentUser
+            if(user != null){
+                if(!hasInitCalled){
+                    hasInitCalled = true
+                    init()
+                }
+            }else{
+                finish()
+                val it = Intent(this, SigninActivity::class.java).apply {
+                    addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK)
+                }
+                startActivity(it)
             }
         }
-        return false
+
+    override fun onStart() {
+        super.onStart()
+        fbAuth.addAuthStateListener(authListener)
     }
 
-    fun newFragment(container: Fragment){
-        val fragment = container
-        supportFragmentManager.beginTransaction().replace(R.id.container, container, fragment.javaClass.simpleName)
-            .commit()
+    override fun onStop() {
+        super.onStop()
+        fbAuth.removeAuthStateListener(authListener)
     }
+
+    protected fun init(){}
 
 }
